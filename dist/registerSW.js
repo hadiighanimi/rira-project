bf3f855f4e066cd7e30f0337db821a4ca697ed12
hadiighanimi <hadighanimi8283@gmail.com>
2026-08-21 16:47:49 +0330

    :Changes-readme & delete-dist

--- dist/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://hadiighanimi.github.io/rira-project/sw.js', { scope: 'https://hadiighanimi.github.io/rira-project/' })})}
